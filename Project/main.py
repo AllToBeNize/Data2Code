527d913204f429c3540120c341cb378bcfd7fb84
AllToBeNize <2184196946@qq.com>
2025-12-27 03:33:45 +0800

    记录导出的命令

--- Project/main.py
+++ Project/main.py
@@ -1,1 +1,3 @@
 # pipreqs ./ --encoding=utf8  --force
+# python -m tools.export "D:\Data2Code\Excel" --lang csharp --data bin --out "D:\Data2Code\Output\Binary" 
+# python -m tools.export "D:\Data2Code\Excel" --lang csharp --data json --out "D:\Data2Code\Output\Json"
